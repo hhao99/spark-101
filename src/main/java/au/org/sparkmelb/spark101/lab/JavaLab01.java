be7c59a4cdf4bedee7bc529fb8f423ccda65406a
hhao99 <hhao99@163.com>
2018-07-12 21:47:04 +0800

    'v0.1'

---
+++ src/main/java/au/org/sparkmelb/spark101/lab/JavaLab01.java
@@ -0,0 +1,32 @@
+package au.org.sparkmelb.spark101.lab;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import java.util.Arrays;
+
+/**
+ * Example program in Java to demonstrate
+ * - Use of textFile to read raw text =
+ * - Use of JavaRDD
+ * - Use of FlatMap
+ *
+ */
+public class JavaLab01 {
+
+    public static void main(String[] argsc) {
+
+        // Setup Spark Configuration
+
+        // Define Java Spark Context
+
+        // Perform the read from file creating an RDD
+
+        // Lets do something different, do a word count
+
+        // Display results
+
+    }
+}
+
---
+++ src/main/scala/com/vw/ScalaLab01.scala
@@ -0,0 +1,28 @@
+package com.vw
+// Import libraries
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+/**
+ * @author timothy.findlay
+ */
+object ScalaLab01 {
+
+  def main(args: Array[String]) {
+
+    // Setup Spark Configuration
+  
+    // Define Spark Context
+    
+    // Setup SQL Context to get Dataframe
+  
+    // Define Filename, store in a static variable fileName
+
+    // Perform the read
+     
+    // Display results
+
+  }
+
+}
